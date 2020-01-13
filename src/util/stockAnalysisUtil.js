14233b33321c8b270fed0d6e9d531b7e14043807
xiaobxia <673806687@qq.com>
2020-01-13 19:24:48 +0800

    xg

--- src/util/stockAnalysisUtil.js
+++ src/util/stockAnalysisUtil.js
@@ -2,6 +2,7 @@ export default {
   // 根据20天线判断是否锁仓
   ifNoSell: function (averageList) {
     let last = averageList[averageList.length - 1]
+    let lastTwo = averageList[averageList.length - 2]
     if (last > 0) {
       let max = 0
       let maxIndex = 0
@@ -13,7 +14,7 @@ export default {
         }
       }
       if (max <= 0) {
-        if (averageList[averageList.length - 2] > 0) {
+        if (lastTwo > 0) {
           return true
         }
         return false
@@ -21,9 +22,7 @@ export default {
         for (let j = maxIndex; j < averageList.length; j++) {
           let now = averageList[j]
           if (now < (max * 0.5)) {
-            if (now >= 0.1) {
-              return false
-            }
+            return false
           }
         }
         return true

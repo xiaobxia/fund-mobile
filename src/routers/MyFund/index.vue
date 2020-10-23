4ea5f8e736805107089f9dd6dd824b33051279cd
xiaobxia <673806687@qq.com>
2020-10-23 11:31:08 +0800

    部署

--- src/routers/MyFund/index.vue
+++ src/routers/MyFund/index.vue
@@ -195,7 +195,7 @@ export default {
         })
         for (let i = 0; i < this.cardInfo.length; i++) {
           this.cardInfo[i].list = dataMap[this.cardInfo[i].name]
-          this.cardInfo[i].list.sort((a, b)=>{
+          this.cardInfo[i].list.sort((a, b) => {
             if (a.theme > b.theme) {
               return 1
             } else {

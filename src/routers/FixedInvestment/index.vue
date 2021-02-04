68f998399b9da978dd257a50b09442d5c64d3504
xiaobxia <673806687@qq.com>
2021-02-04 17:23:32 +0800

    xg

--- src/routers/FixedInvestment/index.vue
+++ src/routers/FixedInvestment/index.vue
@@ -510,7 +510,7 @@ export default {
   },
   methods: {
     ifRemoveFix (key) {
-      if (['jisuanji', 'xinxi', 'yiqian'].indexOf(key) !== -1) {
+      if (['jisuanji', 'xinxi', 'yiqian', 'huangjin'].indexOf(key) !== -1) {
         return true
       }
       return false

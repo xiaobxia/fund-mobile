df278ff22b7af4af38f49844ee7a389d284083ca
xiaobxia <673806687@qq.com>
2021-04-28 09:54:37 +0800

    xg

--- src/routers/BtbIndex/index.vue
+++ src/routers/BtbIndex/index.vue
@@ -9,9 +9,11 @@
       <div>日线是从8点开始的</div>
       <div class="r">
         <div>比特币当前价：{{btbClose}}</div>
+        <div>5/10偏差：<span :class="stockNumberClass(btbDiff)">{{btbDiff}} ({{btbDiff > 0 ? '买' : '卖'}})</span></div>
       </div>
       <div class="r">
         <div>以太坊当前价：{{ethClose}}</div>
+        <div>5/10偏差：<span :class="stockNumberClass(ethDiff)">{{ethDiff}} ({{ethDiff > 0 ? '买' : '卖'}})</span></div>
       </div>
     </div>
   </div>
@@ -19,26 +21,63 @@
 
 <script>
 import {Indicator} from 'mint-ui'
+import numberUtil from '@/util/numberUtil.js'
+
+function getAverage (netValue, day, index) {
+  let start = index - day + 1
+  start = start < 0 ? 0 : start
+  let count = 0
+  for (let i = index; i >= start; i--) {
+    count += netValue[i]['close']
+  }
+  return numberUtil.keepTwoDecimals(count / (index + 1 - start))
+}
 
 export default {
   name: 'BtbIndex',
   data () {
     return {
       btbClose: 0,
-      ethClose: 0
+      ethClose: 0,
+      btbDiff: 0,
+      ethDiff: 0
     }
   },
   watch: {
   },
   created () {
     this.queryBtbKlines()
+    this.queryETHKlines()
   },
   methods: {
+    getAverageList (netValue, day) {
+      const list = []
+      const newList = []
+      netValue.forEach((item) => {
+        newList.unshift(item)
+      })
+      newList.forEach((item, index) => {
+        const average = getAverage(newList, day, index)
+        list.push(average)
+      })
+      return list
+    },
     queryBtbKlines () {
       return this.$http.get('stock/getBtbKlines').then((res) => {
         const list = res.data || []
         const now = list[list.length - 1]
         this.btbClose = now.close
+        // 计算
+        const newList = []
+        list.forEach((item) => {
+          item.netChangeRatio = this.countDifferenceRate(item.close, item.open)
+          newList.push(item)
+        })
+        newList.reverse()
+        const list5 = this.getAverageList(newList, 5)
+        const list10 = this.getAverageList(newList, 10)
+        const lastIndex = list5.length - 1
+        this.btbDiff = this.countDifferenceRate(list5[lastIndex], list10[lastIndex])
       })
     },
     queryETHKlines () {
@@ -46,6 +85,17 @@ export default {
         const list = res.data || []
         const now = list[list.length - 1]
         this.ethClose = now.close
+        // 计算
+        const newList = []
+        list.forEach((item) => {
+          item.netChangeRatio = this.countDifferenceRate(item.close, item.open)
+          newList.push(item)
+        })
+        newList.reverse()
+        const list5 = this.getAverageList(newList, 5)
+        const list10 = this.getAverageList(newList, 10)
+        const lastIndex = list5.length - 1
+        this.ethDiff = this.countDifferenceRate(list5[lastIndex], list10[lastIndex])
       })
     },
     backHandler () {

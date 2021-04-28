e8bda6acf0d21d63044671ea18a6a8dc827d2b5d
xiaobxia <673806687@qq.com>
2021-04-28 09:41:05 +0800

    xg

--- src/routers/BtbIndex/index.vue
+++ src/routers/BtbIndex/index.vue
@@ -6,14 +6,12 @@
       </mt-button>
     </mt-header>
     <div class="main-body">
+      <div>日线是从8点开始的</div>
       <div class="r">
-        <div>当前价：{{close}}</div>
+        <div>比特币当前价：{{btbClose}}</div>
       </div>
-      <div>
-        <div v-for="(item, index) in list" :key="index" class="type-card" @click="toEdit(item)">
-          <h4>{{item.type}}</h4>
-          <div>份额：{{item.shares}}</div>
-        </div>
+      <div class="r">
+        <div>以太坊当前价：{{ethClose}}</div>
       </div>
     </div>
   </div>
@@ -26,35 +24,28 @@ export default {
   name: 'BtbIndex',
   data () {
     return {
-      close: 0
+      btbClose: 0,
+      ethClose: 0
     }
   },
   watch: {
   },
   created () {
     this.queryBtbKlines()
-    this.queryList()
   },
   methods: {
     queryBtbKlines () {
       return this.$http.get('stock/getBtbKlines').then((res) => {
         const list = res.data || []
         const now = list[list.length - 1]
-        this.close = now.close
+        this.btbClose = now.close
       })
     },
-    queryList () {
-      return this.$http.get('btb/getUserBtbs').then((res) => {
-        const list = res.data.list || []
-        list.forEach((v) => {
-          let shares = 0
-          v.position_record = v.position_record || []
-          v.position_record.forEach((p) => {
-            shares += p.shares || 0
-          })
-          v.shares = shares
-        })
-        this.list = list
+    queryETHKlines () {
+      return this.$http.get('stock/getETHKlines').then((res) => {
+        const list = res.data || []
+        const now = list[list.length - 1]
+        this.ethClose = now.close
       })
     },
     backHandler () {
